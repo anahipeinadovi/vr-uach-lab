7f219c3b5c7d98ee9aa6a0487d2bca1d87de7b2a
anahip2602 <anahi.peinadovillalobos26@gmail.com>
2023-09-28 23:49:12 -0600

    se agregan barras de navegacion

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { MenuItem } from 'primeng/api';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,88 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'paginaservicio';
+
+  items: MenuItem[] | undefined;
+
+  menuItems2: MenuItem[] | undefined;
+
+  ngOnInit() {
+      this.items = [
+          {
+              label: 'Inicio',
+              icon: 'pi pi-fw pi-home',
+          },
+          {
+              label: 'Desarrollo VR',
+              icon: 'pi pi-fw pi-desktop',
+          },
+          {
+              label: 'Proyectos',
+              icon: 'pi pi-fw pi-pencil',
+          },
+          {
+              label: 'Nosotros',
+              icon: 'pi pi-fw pi-user',
+          },
+      ];
+
+
+      this.menuItems2 = [
+        {
+            label: 'Inicio',
+            icon: 'pi pi-fw pi-home',
+            items: [
+                {
+                    label: '¿Qué es VR?'
+                },
+                {
+                    label: 'Unity'
+                },
+                {
+                    label: 'Gafas de Realidad virtual'
+                }
+            ]
+        },
+        {
+            label: 'Desarrollo VR',
+            icon: 'pi pi-fw pi-desktop',
+            items: [
+                {
+                    label: 'Curso Unity'
+                },
+                {
+                    label: 'Herramientas'
+                },
+                {
+                    label: 'Cuidados-salud-servicio de seguridad'
+                }
+            ]
+        },
+        {
+            label: 'Proyectos',
+            icon: 'pi pi-fw pi-pencil',
+            items: [
+                {
+                    label: 'Proy 1 Carro'
+                }
+            ]
+        },
+        {
+            label: 'Nosotros',
+            icon: 'pi pi-fw pi-user',
+            items: [
+                {
+                    label: 'Creadores proy 1'
+                }
+            ]
+        }
+    ];
+  }
+
+
+
+
+
+
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,26 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+
+import { MenubarModule } from 'primeng/menubar';
+import { PanelMenuModule } from 'primeng/panelmenu';
+import { CardModule } from 'primeng/card';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    BrowserAnimationsModule,
+    AppRoutingModule,
+    MenubarModule,
+    PanelMenuModule,
+    CardModule
+
+
   ],
   providers: [],
   bootstrap: [AppComponent]
