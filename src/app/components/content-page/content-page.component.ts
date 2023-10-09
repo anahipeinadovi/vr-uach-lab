84f81e7c1821e410260474691bc722ff97299ea2
Fuan200 <juandiazfdez1992@gmail.com>
2023-10-08 18:54:38 -0600

    Modify src

--- src/app/components/content-page/content-page.component.ts
+++ src/app/components/content-page/content-page.component.ts
@@ -8,14 +8,17 @@ import { HttpClient } from '@angular/common/http';
   styleUrls: ['./content-page.component.css']
 })
 export class ContentPageComponent implements OnInit {
-  defaultUrl = "https://raw.githubusercontent.com/Fuan200/info_web_site/main/"
-  name:string|null = ''
+  defaultUrl = "https://raw.githubusercontent.com/Fuan200/info_web_site/main/";
+  name:string|null = '';
+  content: string = '';
   constructor(private route:ActivatedRoute, private http:HttpClient){}
 
   ngOnInit(): void {
-    this.name = this.route.snapshot.paramMap.get('name');
-    //this.http.get(this.defaultUrl + this.name).subscribe({
-     // next:(res) => {}
-    //})
+    this.name = this.route.snapshot.paramMap.get('id');
+    this.http.get(this.defaultUrl + this.name,{responseType:'text'}).subscribe({
+      next:(res) => {
+        this.content = res.toString();
+      }
+    })
   }
 }
